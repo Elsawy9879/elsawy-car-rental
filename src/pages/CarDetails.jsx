fac19045bb8ebf9dbcd65414df93b8244e91af85
Mamdouh Sawy <81479655+Elsawy9879@users.noreply.github.com>
2026-06-01 02:18:35 +0300

    Update CarDetails.jsx

--- src/pages/CarDetails.jsx
+++ src/pages/CarDetails.jsx
@@ -1,30 +1,32 @@
-import { useParams } from 'react-router-dom'
-import Navbar from '../components/Navbar'
-import Footer from '../components/Footer'
-import cars from '../data/cars'
-
 function CarDetails() {
 
   const { id } = useParams()
 
   const car = cars.find((item) => item.id == id)
 
+  if (!car) {
+    return (
+      <>
+        <Navbar />
+        <div style={{ color: "white", padding: "100px" }}>
+          Car Not Found
+        </div>
+        <Footer />
+      </>
+    )
+  }
+
   return (
     <>
       <Navbar />
 
       <section className='details-page'>
-
         <img src={car.image} alt={car.name} />
 
         <div className='details-content'>
-
           <h1>{car.name}</h1>
 
-
-          <p>
-            سيارة حديثة ومناسبة للسفر والتنقل داخل مصر
-          </p>
+          <p>سيارة حديثة ومناسبة للسفر والتنقل داخل مصر</p>
 
           <a
             href={`https://wa.me/201143332696?text=عايز%20احجز%20${car.name}`}
@@ -33,14 +35,10 @@ function CarDetails() {
           >
             تواصل واتساب
           </a>
-
         </div>
-
       </section>
 
       <Footer />
     </>
   )
 }
-
-export default CarDetails
